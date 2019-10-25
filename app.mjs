944b0d28fd0710b369c5664496dd0e3f566fbcae
fnouri1377 <fatemeh@DESKTOP-9NPGTT9.localdomain>
2019-10-26 00:01:12 +0330

    resolve port issue

--- app.mjs
+++ app.mjs
@@ -2,7 +2,7 @@ import express from 'express'
 import turf from '@turf/turf'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 app.use(express.json());
 
 var all_poly_list = [];
