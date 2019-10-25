2851df49185acf092d6611b9bd2bd15b7571e2a2
fnouri1377 <fatemeh@DESKTOP-9NPGTT9.localdomain>
2019-10-25 23:39:57 +0330

    hw1_commit

---
+++ app.mjs
@@ -0,0 +1,37 @@
+import express from 'express'
+import turf from '@turf/turf'
+
+const app = express()
+const port = 3000
+app.use(express.json());
+
+var all_poly_list = [];
+var result_poly_json = {
+    "polygons": []
+};
+
+app.get('/gis/testpoint', (req, res) => {
+    var {lat, long} = req.query;
+    var pt = turf.point([lat, long]);
+    all_poly_list.forEach(poly => {
+        if (turf.booleanPointInPolygon(pt, poly)) {
+            result_poly_json.polygons.push(poly.properties.name);
+        }
+    });
+    res.send(result_poly_json);
+});
+
+app.put('/gis/addpolygon', (req, res) => {
+    var {type} = req.body;
+    if (type === "Feature") {
+        all_poly_list.push(turf.multiPolygon([req.body.geometry.coordinates], req.body.properties));
+    }
+    else if (type === "FeatureCollection") {
+        req.body.features.forEach(poly => {
+            all_poly_list.push(turf.multiPolygon([poly.geometry.coordinates], poly.properties));
+        });
+    }
+    res.send(all_poly_list);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
